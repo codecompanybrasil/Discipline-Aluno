6a867af78d5548a0c381eeb018dab85db88b3b05
Pablo Pereira <pablopereira27@gmail.com>
2023-09-06 11:17:18 -0300

    Inclusão do Discipline Core
    - Inclusão do Discipline Core 1.0.6
    - Remoção de todos os CSS que foram transferidos para o core.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import "./assets/css/bootstrap-reboot.min.css"
-import "./assets/css/bootstrap-grid.min.css"
 import "./App.css";
+
 import Avaliacoes from "./Pages/Avaliacoes/main";
 
 function App() {
